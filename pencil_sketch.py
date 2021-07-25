c12a4ae2cc943851f6c891af053ffec0d554eb70
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-26 03:55:48 +0530

    minor changes and outputs

--- pencil_sketch.py
+++ pencil_sketch.py
@@ -4,7 +4,7 @@ import cv2
 image = cv2.imread("bachpan.jpg")
 
 #Resizing the Image
-image = cv2.resize(image,(1280,720))
+image = cv2.resize(image,(900,800))
 
 #Converting to grayscale
 grayScale = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
@@ -18,10 +18,14 @@ gaussian_blur = cv2.GaussianBlur(image_invert,(23,23),sigmaX = 0,sigmaY=0)
 #Sketch
 sketch_image = cv2.divide(grayScale,255-gaussian_blur,scale = 256)
 
-#Showing the picture in a Window
+#Showing the pictures in a Window
 cv2.imshow("Sketch Image",sketch_image)
+cv2.imshow("Invert Image",image_invert)
+cv2.imshow("GrayScale Image",grayScale)
 
 #Writing in a Image file
 cv2.imwrite("Picture_Sketch.jpg",sketch_image)
+cv2.imwrite("Invert_Image.jpg",image_invert)
+cv2.imwrite("GrayScale_Image.jpg",grayScale)
 
 cv2.waitKey(0)
